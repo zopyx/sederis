3b1b5b1a2402c404e95f29f5a91a06cc6a0a2285
Andreas Jung <info@zopyx.com>
2021-05-01 16:19:24 +0200

    updated

--- html/code.js
+++ html/code.js
@@ -1,28 +1,23 @@
-
 /* MAIN */
 
 function fixes() {
     resize_fonts();
 }
 
-
 function resize_fonts() {
-
-    console.log(ro.layout);
 
     var maxPages = 2;
     var maxIterations = 10;
-
 
     var default_font_size = 11;
     var default_line_height = 1.2;
 
     var count = 0
     while (ro.layout.numberOfPages > maxPages && count < maxIterations) {
-        var nodes = document.querySelectorAll(".autofit");
         default_font_size = default_font_size * 0.99;
         default_line_height = default_line_height * 0.99;
 
+        var nodes = document.querySelectorAll(".autofit");
         for (var i=0; i<nodes.length; i++) {
             var node = nodes[i];
             node.style.fontSize = default_font_size + "pt";
@@ -31,7 +26,4 @@ function resize_fonts() {
         ro.layout.forceRelayout();
         count++;
     }
-
-    console.log(ro.layout);
 }
-
